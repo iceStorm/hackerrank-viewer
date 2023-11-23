1ccb430c7232cc591677e10be5a862a691ae17f7
iceStorm <hauduedaiduong@gmail.com>
2023-11-23 10:38:26 +0700

    Enhance download response

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -44,13 +44,26 @@ export async function GET(req: Request, res: Response) {
   const searchParams = new URLSearchParams(new URL(req.url).search)
   const username = searchParams.get("username")
 
-  const response = await fetch(
+  const response = await axios.get<{ data: Cert[] }>(
     `https://www.hackerrank.com/community/v1/test_results/hacker_certificate?username=${username}`,
+    {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+      },
+    },
   )
 
-  const certs: Cert[] = (await response.json()).data
+  const passedCerts = response.data.data.filter(cert => cert.attributes.status === "test_passed")
 
-  const passedCerts = certs.filter(cert => cert.attributes.status === "test_passed")
+  if (!passedCerts.length) {
+    return Response.json({
+      username,
+      message:
+        "There is no data to download. You have typed the wrong HackerRank username, or your account does not contain any certificates.",
+      profile: `https://www.hackerrank.com/profile/${username}`,
+    })
+  }
 
   const certImage = await Promise.all(
     passedCerts.map(async cert => {
@@ -67,7 +80,10 @@ export async function GET(req: Request, res: Response) {
 
   certImage.forEach((imageBuffer, index) => {
     zipper.addFile(
-      `${passedCerts[index].id}__${passedCerts[index].attributes.certificates[0]}.jpg`,
+      `${passedCerts[index].id}__${passedCerts[index].attributes.certificates[0].replace(
+        " ()",
+        "",
+      )}.jpg`,
       Buffer.from(imageBuffer),
     )
   })
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image"
 
 export default function Home() {
-  return <>Home.</>
+  const bodyText = `<>Hello World.</>`
+
+  return (
+    <div className="container h-screen flex flex-col justify-center items-center">
+      <pre>{bodyText}</pre>
+    </div>
+  )
 }
